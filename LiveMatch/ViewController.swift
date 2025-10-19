ed9776ec65e85218d6e302dd587eb31927c3c613
Boray Chen <r0975929562@gmail.com>
2025-10-19 23:20:34 +0800

    Initial Commit

---
+++ LiveMatch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveMatch
+//
+//  Created by Boray Chen on 2025/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiveMatchTests/LiveMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveMatchTests.swift
+//  LiveMatchTests
+//
+//  Created by Boray Chen on 2025/10/19.
+//
+
+import Testing
+@testable import LiveMatch
+
+struct LiveMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
